3a3d1c37c0342f3fb277fb8f90dabd6c6ef0a8bb
Nantawat <nantawat7183@gmail.com>
2020-01-27 21:05:10 +0700

    v1.1

--- login.php
+++ login.php
@@ -3,37 +3,37 @@
 <!DOCTYPE html>
 
 <html lang="en">
-  <head>
-    <title>ShopVegetable &mdash; Vegetable Shop Online</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <title>ShopVegetable &mdash; Vegetable Shop Online</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <link rel="stylesheet" href="css/jquery-ui.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
 
-    <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/aos.css">
 
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="/lib/w3.css">
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
-  </head>
-  <body>
-  
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
   <div class="site-wrap">
-    
+
 
     <div class="site-navbar bg-white py-2">
 
@@ -49,186 +49,214 @@
 
       
 
-     <?php include "Menu.php"; ?>
-          
-            
+      <?php include "Menu.php"; ?>
 
-    <div class="custom-border-bottom py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Login/Register</strong></div>
+
+
+      <div class="custom-border-bottom py-3">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Login/Register</strong></div>
+          </div>
         </div>
       </div>
-    </div>
 
 
-    <div class="site-section">
-      <div class="container">
-        <div class="row">
-          
+      <div class="site-section">
+        <div class="container">
+          <div class="row">
 
 
-          <div class="col-md-7">
 
-            <form action="adduser.php" method="post">
+            <div class="col-md-7">
+
+              <form action="adduser.php" method="post">
 
 
-              
-              <div class="p-3 p-lg-5 border">
-                <h2 class="h3 mb-3 text-black">สมัคสมาชิก</h2>
-              <div class="form-group row">
-                  <div class="col-md-6">
-                    <label for="f_name" class="text-black">ชื่อ <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="f_name" name="f_name">
 
+                <div class="p-3 p-lg-5 border">
+                  <h2 class="h3 mb-3 text-black">สมัคสมาชิก</h2>
+                  <div class="form-group row">
+                    <div class="col-md-6">
+                      <label for="f_name" class="text-black">ชื่อ <span class="text-danger">*</span></label>
+                      <input type="text" class="form-control" id="f_name" name="f_name" required>
+
+                    </div>
+                    <div class="col-md-6">
+                      <label for="l_name" class="text-black">นามสกุล <span class="text-danger">*</span></label>
+                      <input type="text" class="form-control" id="l_name" name="l_name" required>
+                    </div>
                   </div>
-                  <div class="col-md-6">
-                    <label for="l_name" class="text-black">นามสกุล <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="l_name" name="l_name">
+                  <div class="form-group row">
+                    <div class="col-md-12">
+                      <label for="addess" class="text-black">ที่อยู่ <span class="text-danger">*</span></label>
+                      <input type="text" class="form-control" id="addess" name="addess" placeholder="" required>
+                    </div>
                   </div>
-                </div>
-                 <div class="form-group row">
-                  <div class="col-md-12">
-                    <label for="addess" class="text-black">ที่อยู่ <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="addess" name="addess" placeholder="">
+                  <div class="form-group row">
+                    <div class="col-md-12">
+                      <label for="email" class="text-black">อีเมลล์ <span class="text-danger">*</span></label>
+                      <input type="email" class="form-control" id="email" name="email" placeholder="" required="">
+                    </div>
                   </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-md-12">
-                    <label for="email" class="text-black">อีเมลล์ <span class="text-danger">*</span></label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="">
+                  <div class="form-group row">
+                    <div class="col-md-12">
+                      <label for="email" class="text-black">เบอร์โทรศัพท์ <span class="text-danger">*</span></label>
+                      <input type="tel" class="form-control" id="tel" name="tel" required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="ตัวอย่าง ***-***-****">
+                    </div>
                   </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-md-12">
-                    <label for="email" class="text-black">เบอร์โทรศัพท์ <span class="text-danger">*</span></label>
-                    <input type="tel" class="form-control" id="tel" name="tel" required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="ตัวอย่าง ***-***-****">
+                  <div class="form-group row">
+                    <div class="col-md-12">
+                      <label for="password" class="text-black">รหัสผ่าน <span class="text-danger">*</span></label>
+                      <input type="Password" class="form-control" id="password" name="password" required>
+                    </div>
                   </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-md-12">
-                    <label for="password" class="text-black">รหัสผ่าน <span class="text-danger">*</span></label>
-                    <input type="Password" class="form-control" id="password" name="password">
-                  </div>
-                </div>
 
-                <div class="form-group row">
-                  <div class="col-lg-12">
-                    <input type="submit" class="btn btn-primary btn-lg btn-block" value="สมัคสมาชิก">
+                  <div class="form-group row">
+                    <div class="col-lg-12">
+                      <input type="submit" class="btn btn-primary btn-lg btn-block" value="สมัคสมาชิก">
+                    </div>
                   </div>
-                </div>
 
-              </div>
-            </form>
-
-          </div>
-
-          <div class="col-md-5 ml-auto">
-
-           <form action="check_login.php" method="post">
-            <div class="p-3 p-lg-5 border">
-              <h2 class="h3 mb-3 text-black">ล็อกอิน</h2>
-                <div class="form-group row">
-                  <div class="col-md-12">
-                    <label for="email" class="text-black">อีเมลล์ <span class="text-danger">*</span></label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-md-12">
-                    <label for="password" class="text-black">รหัสผ่าน <span class="text-danger">*</span></label>
-                    <input type="Password" class="form-control" id="password" name="password">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-lg-12">
-                    <input type="submit" class="btn btn-primary btn-lg btn-block" value="LOGIN">
-                  </div>
                 </div>
               </form>
 
-          </div>
+            </div>
 
+            <div class="col-md-5 ml-auto">
+
+             <form action="check_login.php" method="post">
+              <div class="p-3 p-lg-5 border">
+                <h2 class="h3 mb-3 text-black">ล็อกอิน</h2>
+                <div class="form-group row">
+                  <div class="col-md-12">
+                    <label for="email" class="text-black">อีเมลล์ <span class="text-danger" >*</span></label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="อีเมลล์" required>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-md-12">
+                    <label for="password" class="text-black">รหัสผ่าน <span class="text-danger">*</span></label>
+                    <input type="Password" class="form-control" id="password" name="password"  placeholder="รหัสผ่าน" required>
+                  </div>
+                </div>
+                <div class="form-group row">
+                  <div class="col-lg-12">
+                    <input type="submit" class="btn btn-primary btn-lg btn-block" value="เข้าสู้ระบบ">
+                  </div>
+                </div>
+              </form>
+              <a href="#" data-toggle="modal" data-target="#exampleModalCenter"/>ลืมรหัสผ่าน </a>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">ลืมรหัสผ่าน</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="send_mail.php" method="post">
+          <div class="form-group row">
+            <div class="col-md-12">
+              <label for="email" class="text-black">อีเมลล์ <span class="text-danger">*</span></label>
+              <input type="email" class="form-control" id="email" name="email" placeholder="">
+            </div>
+          </div>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">ยืนยัน</button>
         </div>
       </div>
     </div>
+  </div>
 
 
-    <footer class="site-footer custom-border-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
-            <h3 class="footer-heading mb-4">OUR SHOP</h3>
-            <a href="#" class="block-6">
-              <img src="images/pic.png" alt="Image placeholder" class="img-fluid rounded mb-4">
-              <h3 class="font-weight-light  mb-0">A quality vegetable shop that you can't find anywhere else.</h3>
-              <p>Open now  &mdash;  April 20, 2019</p>
-            </a>
-          </div>
-          <div class="col-lg-5 ml-auto mb-5 mb-lg-0">
-            <div class="row">
-              <div class="col-md-12">
-                <h3 class="footer-heading mb-4">Quick Links</h3>
-              </div>
-              <div class="col-md-6 col-lg-4">
-                <ul class="list-unstyled">
-                  <li><a href="#">Sell online</a></li>
-                  <li><a href="#">Features</a></li>
-                  <li><a href="#">Shopping cart</a></li>
-                  <li><a href="#">Store builder</a></li>
-                </ul>
-              </div>
-              <div class="col-md-6 col-lg-4">
-                <ul class="list-unstyled">
-                  <li><a href="#">Mobile commerce</a></li>
-                  <li><a href="#">Dropshipping</a></li>
-                  <li><a href="#">Website development</a></li>
-                </ul>
-              </div>
-              <div class="col-md-6 col-lg-4">
-                <ul class="list-unstyled">
-                  <li><a href="#">Point of sale</a></li>
-                  <li><a href="#">Hardware</a></li>
-                  <li><a href="#">Software</a></li>
-                </ul>
-              </div>
+  <footer class="site-footer custom-border-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+          <h3 class="footer-heading mb-4">OUR SHOP</h3>
+          <a href="#" class="block-6">
+            <img src="images/pic.png" alt="Image placeholder" class="img-fluid rounded mb-4">
+            <h3 class="font-weight-light  mb-0">A quality vegetable shop that you can't find anywhere else.</h3>
+            <p>Open now  &mdash;  April 20, 2019</p>
+          </a>
+        </div>
+        <div class="col-lg-5 ml-auto mb-5 mb-lg-0">
+          <div class="row">
+            <div class="col-md-12">
+              <h3 class="footer-heading mb-4">Quick Links</h3>
             </div>
-          </div>
-          
-          <div class="col-md-6 col-lg-3">
-            <div class="block-5 mb-5">
-              <h3 class="footer-heading mb-4">Contact Info</h3>
+            <div class="col-md-6 col-lg-4">
               <ul class="list-unstyled">
-                <li class="address">123/2001 Information Technology, Department of Computer Science, Faculty of Science, KKU</li>
-                <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
-                <li class="email">n.kanrutai@kkumail.com</li>
+                <li><a href="#">Sell online</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Shopping cart</a></li>
+                <li><a href="#">Store builder</a></li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <ul class="list-unstyled">
+                <li><a href="#">Mobile commerce</a></li>
+                <li><a href="#">Dropshipping</a></li>
+                <li><a href="#">Website development</a></li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <ul class="list-unstyled">
+                <li><a href="#">Point of sale</a></li>
+                <li><a href="#">Hardware</a></li>
+                <li><a href="#">Software</a></li>
               </ul>
             </div>
           </div>
         </div>
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <p>
+
+        <div class="col-md-6 col-lg-3">
+          <div class="block-5 mb-5">
+            <h3 class="footer-heading mb-4">Contact Info</h3>
+            <ul class="list-unstyled">
+              <li class="address">123/2001 Information Technology, Department of Computer Science, Faculty of Science, KKU</li>
+              <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
+              <li class="email">n.kanrutai@kkumail.com</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row pt-5 mt-5 text-center">
+        <div class="col-md-12">
+          <p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             Copyright &copy;<script>document.write(new Date().getFullYear());</script> Khon Kaen University
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-          </div>
-          
+          </p>
         </div>
+
       </div>
-    </footer>
-  </div>
+    </div>
+  </footer>
+</div>
 
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/aos.js"></script>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
 
-  <script src="js/main.js"></script>
-    
-  </body>
+<script src="js/main.js"></script>
+
+</body>
 </html>
--- send_mail.php
+++ send_mail.php
@@ -1,22 +1,56 @@
-<html>
-<head>
-<title>ThaiCreate.Com PHP Sending Email</title>
-</head>
-<body>
+<?php include "connectdb.php";?>
 <?php
-	$strTo = "jaruwat.m@kkumail.com";
-	$strSubject = "Test Send Email";
-	$strHeader = "From: nantawat7183@gmail.com";
-	$strMessage = "My Body & My Description";
-	$flgSend = @mail($strTo,$strSubject,$strMessage,$strHeader);  // @ = No Show Error //
-	if($flgSend)
-	{
-		echo "Email Sending.";
-	}
-	else
-	{
-		echo "Email Can Not Send.";
-	}
+$ses_userid = $_POST["email"];
+$sql = "SELECT * FROM user WHERE email='".$ses_userid."'";
+$objQuery = mysqli_query($conn, $sql);
+$objResult = mysqli_fetch_array($objQuery);
+$f_name = $objResult["f_name"];
+$l_name = $objResult["l_name"];
+$password = $objResult["password"];
+$email = $objResult["email"];
+
+
+$to = $email;
+$subject = "My subject";
+$txt = "รหัสผ่านของคุณคือ".$password;
+$headers = "From: nantawat7183@gmail.com";
+
+if(mail($to,$subject,$txt,$headers))
+
+{
+	 echo "<!DOCTYPE html>
+	<head>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Kanit|Prompt'>
+		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+		<link rel='stylesheet' href='css/login.css'>
+	<body style='padding:350px'>
+		<center><h2>ระบบได้ส่งรหัสผ่านไปยังอีเมลล์ของคุณ เรียบร้อยเเล้ว</h2>
+		
+		<a href='login.php'>กลับสู่หน้าหลัก</a></center>
+	</body>
+	</head>
+</html>";
+	
+
+}
+else
+{
+	echo "Email Can Not Send.";
+}
+
+
+// $to_email = "jaruwat.m@kkumail.com";
+// $subject = "Simple Email Test via PHP";
+// $body = "Hi,nn This is test email send by PHP Script";
+// $headers = "From: nantawat7183@gmail.com";
+
+// if (mail($to_email, $subject, $body, $headers)) {
+//     echo "Email successfully sent to $to_email...";
+// } else {
+//     echo "Email sending failed...";
+// }
+
+
+
 ?>
-</body>
-</html>
